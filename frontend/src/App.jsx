794c1f556d43085c38a65f1d12ed7e996c5bc34f
amr512 <amr512hossam@gmail.com>
2024-07-02 14:59:55 +0300

    change router to use useRoutes()

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes, useLocation, useRoutes } from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
 import "./App.css"; // Import your CSS animations
 import Home from "./pages/Home";
@@ -13,46 +13,61 @@ import NavBar from "./components/NavBar";
 
 const App = () => {
   const Location = useLocation();
-  ColorModeProvider({
-    colorModeManager: localStorageManager,
-    options: { initialColorMode: "system" },
-  });
+  // ColorModeProvider({
+  //   colorModeManager: localStorageManager,
+  //   options: { initialColorMode: "system" },
+  // });
 
+  // const navigate = useNavigate();
+  
   const animate = (Component) => {
-    return function anim() {
+    // return function anim() {
       return (
         <motion.div
-          key={Location.key}
-          initial={{ translateX: 100, opacity: 0.5 }}
-          animate={{
-            translateX: 0,
-            translateY: 0,
-            opacity: 1,
-          }}
-          transition={{
-            type: "tween",
-          }}
-          // exit={{ translateX: -100, opacity: 0}}
+        key={Location.key}
+        initial={{ translateX: 100, opacity: 0.5 }}
+        animate={{
+          translateX: 0,
+          translateY: 0,
+          opacity: 1,
+        }}
+        transition={{
+          type: "tween",
+        }}
+        // exit={{ translateX: -100, opacity: 0}}
         >
           <Component />
         </motion.div>
       );
+      // };
     };
-  };
-
-  return (
-    <div className="app">
+    
+    const routes = useRoutes([
+      {
+        path: "/",
+        element: animate(Home),
+      },
+      {
+        path: "/products",
+        element: animate(ProductsPage),
+      },
+      {
+        path: "*",
+        element: animate(Home),
+      }
+    ],Location)
+    
+    
+    return (
+      <div className="app">
       <NavBar />
       <AnimatePresence >
-        <Routes location={Location} key={Location.pathname}>
-          <Route exact path="/" Component={animate(Home)} />
-          <Route path="/products" Component={animate(ProductsPage)} />
-          {/* <Route path="/contact" component={Contact} /> */}
-          {/* <redirect to="/" /> */}
-        </Routes>
+        {/* <Routes location={Location} key={Location.pathname} > */}
+          {routes}
+        {/* </Routes> */}
       </AnimatePresence>
     </div>
   );
 };
-
+  
 export default App;
--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -2,32 +2,57 @@ import PropTypes from "prop-types";
 import "./styles/ProductCard.css";
 import { API_URL } from "../helpers/constants";
 import formatCurrency from "../helpers/formatCurrency";
-import { Button, Card, CardHeader, Input, Image, Heading, StepSeparator } from "@chakra-ui/react";
+import {
+  Button,
+  Card,
+  CardHeader,
+  Input,
+  Image,
+  Heading,
+  StepSeparator,
+  CardBody,
+  CardFooter,
+} from "@chakra-ui/react";
 export default function ProductCard({ product }) {
   return (
     <div>
-        <Card className="product-card">
-          <CardHeader>
-          <Heading size={'md'}>{product.name}</Heading>
-          <Image className="product-list__image" src={product.images[0]} alt={product.name}/>
-          </CardHeader>
-        <div className="product-list__details">
-          <p>{product.description}</p>
-          <p>Price: {formatCurrency(product.price.value/100, product.price.currency)}</p>
-        </div>
-        <form
-          action={`${API_URL}/create-checkout-session/?price_id=${product.default_price}&product_id=${product.id}&quantity=1`}
-          method="POST"
-          >
-          <div className="flex-row">
-            <Input type="hidden" name="product_id" value={product.id} />
-            <Input type="hidden" name="price_id" value={product.default_price} />
-            <Input type="number" name="quantity" defaultValue={1} />
-          <Button type="submit">Buy</Button>
+      <Card className="product-card">
+        <CardBody>
+          <Heading size={"md"}>{product.name}</Heading>
+          <Image
+            className="product-list__image"
+            src={product.images[0]}
+            alt={product.name}
+            dropShadow={"2xl"}
+          />
+          <div className="product-list__details">
+            <p>{product.description}</p>
+            <p>
+              Price:{" "}
+              {formatCurrency(
+                product.price.value / 100,
+                product.price.currency
+              )}
+            </p>
           </div>
-        </form>
-          </Card>
-      
+
+          <form
+            action={`${API_URL}/create-checkout-session/?price_id=${product.default_price}&product_id=${product.id}&quantity=1`}
+            method="POST"
+            >
+            <div className="flex-row">
+              <Input type="hidden" name="product_id" value={product.id} />
+              <Input
+                type="hidden"
+                name="price_id"
+                value={product.default_price}
+                />
+              <Input type="number" name="quantity" defaultValue={1} />
+              <Button type="submit">Buy</Button>
+            </div>
+          </form>
+                </CardBody>
+      </Card>
     </div>
   );
 }
